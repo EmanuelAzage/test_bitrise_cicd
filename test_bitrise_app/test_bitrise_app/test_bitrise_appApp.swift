a55df7791520730c29ebdbe4745d9cdcce1250bb
Emanuel Azage <eazage@usengineeringsolutions.com>
2023-05-16 14:17:43 -0400

    Adds empty app

---
+++ test_bitrise_app/test_bitrise_app/test_bitrise_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_bitrise_appApp.swift
+//  test_bitrise_app
+//
+//  Created by Emanuel Azage on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct test_bitrise_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
